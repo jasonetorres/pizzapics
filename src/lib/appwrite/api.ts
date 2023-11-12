2619710a8298a9b18a5db0ff95f483cadf0cf480
jason torres <jasontorres585@icloud.com>
2023-11-12 06:07:48 -0500

    update api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,13 +1,10 @@
-import { ID, Query } from "appwrite";
-
+import { ID, Query } from "appwrite"
 import { appwriteConfig, account, databases, storage, avatars } from "./config";
 import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
 
-// ============================================================
 // AUTH
-// ============================================================
 
-// ============================== SIGN UP
+// ==== SIGN UP
 export async function createUserAccount(user: INewUser) {
   try {
     const newAccount = await account.create(
@@ -36,7 +33,7 @@ export async function createUserAccount(user: INewUser) {
   }
 }
 
-// ============================== SAVE USER TO DB
+// === SAVE USER TO DB
 export async function saveUserToDB(user: {
   accountId: string;
   email: string;
@@ -58,7 +55,7 @@ export async function saveUserToDB(user: {
   }
 }
 
-// ============================== SIGN IN
+// ====== SIGN IN
 export async function signInAccount(user: { email: string; password: string }) {
   try {
     const session = await account.createEmailSession(user.email, user.password);
@@ -69,7 +66,7 @@ export async function signInAccount(user: { email: string; password: string }) {
   }
 }
 
-// ============================== GET ACCOUNT
+// ======GET ACCOUNT
 export async function getAccount() {
   try {
     const currentAccount = await account.get();
--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,20 +1,19 @@
-import { Client, Account, Databases, Storage, Avatars } from 'appwrite';
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-    url: import.meta.env.VITE_APPWRITE_URL,
-    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
-    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
-    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
-    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
-}
-
+  url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+};
 
 export const client = new Client();
 
-client.setProject(appwriteConfig.projectId);
 client.setEndpoint(appwriteConfig.url);
+client.setProject(appwriteConfig.projectId);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
