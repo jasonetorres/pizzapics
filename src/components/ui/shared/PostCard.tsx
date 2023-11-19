e0a09b62bbf96d6954ceaec8c623896bd5b18312
jason torres <jasontorres585@icloud.com>
2023-11-19 06:29:54 -0500

    poststats.tsx built

--- src/components/ui/shared/PostCard.tsx
+++ src/components/ui/shared/PostCard.tsx
@@ -1,36 +1,75 @@
+import { useUserContext } from "@/context/AuthContext";
 import { Models } from "appwrite";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
 type PostCardProps = {
-    post: Models.Document;
-}
-const PostCard = ({ post } : PostCardProps) => {
+  post: Models.Document;
+};
+const PostCard = ({ post }: PostCardProps) => {
+  const { user } = useUserContext();
+  if (!post.creator) return;
   return (
     <div className="post-card">
-        <div className="flex-between">
-            <div className="flex items-center gap-3">
-            <Link to={`/profile/${post.creator.$id}`}>
-                <img
-                src={post?.creator?.imageUrl || 'assets/icons/profile-placeholder.svg'}
-                alt="creator"
-                className="rounded-full w-12 lg:h-12"/>
-            </Link>
-            <div className="flex flex-col">
-                <p className="base-medium lg:body-bold text-light-1">
-                    {post.creator.name}
-                </p>
+      <div className="flex-between">
+        <div className="flex items-center gap-3">
+          <Link to={`/profile/${post.creator.$id}`}>
+            <img
+              src={
+                post?.creator?.imageUrl ||
+                "assets/icons/profile-placeholder.svg"
+              }
+              alt="creator"
+              className="rounded-full w-12 lg:h-12"
+            />
+          </Link>
+          <div className="flex flex-col">
+            <p className="base-medium lg:body-bold text-light-1">
+              {post.creator.name}
+            </p>
             <div className="flex-center gap-2 text-light-3">
-                <p className="subtle-semibold lg:small-regular">
-                    {post.$createdAt}
-                </p>
-                -
-                <p className="subtle-semibold lg:small-regular">
-                    {post.location}
-                </p>
+              <p className="subtle-semibold lg:small-regular">
+                {post.$createdAt}
+              </p>
+              -
+              <p className="subtle-semibold lg:small-regular">
+                {post.location}
+              </p>
             </div>
-            </div>
-            </div>
+          </div>
         </div>
+        <Link
+          to={`/update-post/${post.$id}`}
+          className={`${user.id !== post.creator.$id && "hidden"}`}
+        >
+          <img
+            src={"/assets/icons/edit.svg"}
+            alt="edit"
+            width={20}
+            height={20}
+          />
+        </Link>
+      </div>
+      <Link to={"/posts/${post.$id}"}>
+        <div className="small-medium lg:base-medium py-5">
+          <p>{post.caption}</p>
+          <ul className="flex gap-1 mt-2">
+            {post.tags.map((tag: string) => (
+              <li key={tag} className="text-light-3">
+                #{tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <img
+          src={post.imageURL || "/assets/icons/profle-placeholder.svg"}
+          alt="post image"
+          className="post-card_img"
+        />
+      </Link>
+
+      <PostStats post={post} userId={user.id} />
     </div>
-  )};
+  );
+};
 export default PostCard;
